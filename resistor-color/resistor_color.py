2ac6be4b76986ae51660e7c2f25fb076ee017ed2
Enrique Martínez <enri.fc.98@gmail.com>
2022-06-10 11:30:42 +0200

    solved: resistor-color

--- resistor-color/resistor_color.py
+++ resistor-color/resistor_color.py
@@ -1,6 +1,21 @@
+COLORS = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+    }
+
 def color_code(color):
-    pass
+    if color.lower() in COLORS.keys():
+        return COLORS[color.lower()]
+    return
 
 
 def colors():
-    pass
+    return list(COLORS.keys())
